e076b6abfae7595fb649baacf09405668d73dd85
RioMando <roblesrioarmand@gmail.com>
2018-02-14 14:10:17 -0600

    Inventory display

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Bamazon
+
+## Application to manage inventory in a web base store.
+### This app runs for a client side, where you can consult the inventory of the store, price, and quantity available.
+
+#### Because this app runs in console, you need to run it ising node.
+To run the app type:
+node bamazonCustomer < enter >
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var express = require('express');
+var inquirer = require('inquirer');
 
 // Server app setup
 var app = express();
@@ -15,5 +16,42 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log('Connected as id ' + connection.threadId);
+	console.log('\n\n');
+	allProducts();
+	// productToBuy();
 });
 
+function allProducts() {
+	var query = connection.query('SELECT * FROM products;', function(err, res) {
+		console.log('--------------------------------------------------');
+
+		for (var i = 0; i < res.length; i++){
+			console.log(res[i].item_id + " | " + res[i].product_name + ' | ' + res[i].price);
+			
+		}
+		console.log('--------------------------------------------------');
+	productToBuy();
+	})
+	
+}
+
+function productToBuy() {
+	inquirer
+		.prompt({
+			name: "id",
+			type: "input",
+			message: "Which ID number do you want to purchase?"
+		 },
+		 {
+			name: "quantity",
+			type: "input",
+			message: "How many units do you want to buy?"
+		})
+		.then(function(answer){
+			console.log(answer);
+			console.log(answer[0].id);
+			console.log(answer[1].quantity);
+		})
+		
+};
+	
