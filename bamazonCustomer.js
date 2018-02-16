9c68a407dbeaf03347481544a9fb39e4b05c0fad
RioMando <roblesrioarmand@gmail.com>
2018-02-15 21:44:00 -0600

    Function to validate quantities to purchase working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,7 +19,28 @@ connection.connect(function(err) {
 	console.log('\n\n');
 	allProducts();
 	// productToBuy();
+	// processAnswers();
 });
+
+//--------------------------------
+var questions = [
+	{
+		message: "Which ID number do you want to purchase?",
+		type: "input",
+		name: "id",
+	    validate: validateName
+	},{
+		message: "How many units do you want to buy?",
+		type: "input",
+		name: "quantity",
+	    validate: validateName
+	}
+];
+
+function validateName(name){
+        return name !== '';
+};
+//----------------------------------
 
 function allProducts() {
 	var query = connection.query('SELECT * FROM products;', function(err, res) {
@@ -28,30 +49,38 @@ function allProducts() {
 		for (var i = 0; i < res.length; i++){
 			console.log(res[i].item_id + " | " + res[i].product_name + ' | ' + res[i].price);
 			
-		}
-		console.log('--------------------------------------------------');
+		};
+		console.log('--------------------------------------------------\n');
 	productToBuy();
-	})
+	});
 	
-}
+};
 
 function productToBuy() {
-	inquirer
-		.prompt({
-			name: "id",
-			type: "input",
-			message: "Which ID number do you want to purchase?"
-		 },
-		 {
-			name: "quantity",
-			type: "input",
-			message: "How many units do you want to buy?"
-		})
-		.then(function(answer){
-			console.log(answer);
-			console.log(answer[0].id);
-			console.log(answer[1].quantity);
-		})
-		
+	inquirer.prompt(questions, validateName)
+	.then(function(anser){
+		// console.log(anser);
+		// console.log(anser.id);
+		// console.log(anser.quantity);
+		findProduct(anser.id, anser.quantity);
+	});
+};
+
+function findProduct(id, quantity){
+	console.log(id + "   " + quantity);
+	var query = connection.query('SELECT stock_quantity FROM products WHERE item_id=?', [id], function(err, res) {
+		var qtty = JSON.stringify(res[0].stock_quantity);
+		console.log(qtty);
+		if (quantity<=qtty){
+			purchase(id, quantity);
+		} else {
+			console.log("insufficient quantity!\n\n");
+			allProducts();
+		};
+	});
+};
+
+function purchase(id, quantity){
+	console.log("Processing your purchase.");
 };
 	
