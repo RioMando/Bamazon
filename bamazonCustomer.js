63aee337316c2fa84951d62309813dc89534ba39
RioMando <roblesrioarmand@gmail.com>
2018-02-15 23:37:21 -0600

    Challenge 1 complete. Customere app working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,16 +13,17 @@ var connection = mysql.createConnection({
 	password: 'rootroot',
 	database: "bamazon_DB"
 });
+
+//Stablish the connection with bamazon_DB and lounch the program with allProducts()
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log('Connected as id ' + connection.threadId);
 	console.log('\n\n');
 	allProducts();
-	// productToBuy();
-	// processAnswers();
 });
 
-//--------------------------------
+var inventory = 0;
+//Variable used with inquirer to ask the ID and quantity to purchase
 var questions = [
 	{
 		message: "Which ID number do you want to purchase?",
@@ -37,42 +38,41 @@ var questions = [
 	}
 ];
 
+//This function validates that the user type something (not enter without data)
 function validateName(name){
         return name !== '';
 };
-//----------------------------------
 
+//Display of all products fron bamazon_DB displaying: ID, Name and Price
 function allProducts() {
 	var query = connection.query('SELECT * FROM products;', function(err, res) {
-		console.log('--------------------------------------------------');
+		console.log('\n--------------------------------------------------');
 
 		for (var i = 0; i < res.length; i++){
-			console.log(res[i].item_id + " | " + res[i].product_name + ' | ' + res[i].price);
-			
+			console.log(res[i].item_id + " | " + res[i].product_name + ' | ' + "$ " + res[i].price);
 		};
 		console.log('--------------------------------------------------\n');
 	productToBuy();
 	});
-	
 };
 
+//Ask to the client wich product ID and quantity wants to buy
 function productToBuy() {
 	inquirer.prompt(questions, validateName)
-	.then(function(anser){
-		// console.log(anser);
-		// console.log(anser.id);
-		// console.log(anser.quantity);
-		findProduct(anser.id, anser.quantity);
+	.then(function(answer){
+		// console.log(answer);
+		// console.log(answer.id);
+		// console.log(answer.quantity);
+		findProduct(answer.id, parseInt(answer.quantity));
 	});
 };
 
+//Function that look in bamazon_DB for the ID requested by the client and cjeck that there is enof quantity to be sold.
 function findProduct(id, quantity){
-	console.log(id + "   " + quantity);
 	var query = connection.query('SELECT stock_quantity FROM products WHERE item_id=?', [id], function(err, res) {
-		var qtty = JSON.stringify(res[0].stock_quantity);
-		console.log(qtty);
-		if (quantity<=qtty){
-			purchase(id, quantity);
+		inventory = parseInt(JSON.stringify(res[0].stock_quantity));
+		if (quantity <= inventory){
+			updateQuantity(id, quantity);
 		} else {
 			console.log("insufficient quantity!\n\n");
 			allProducts();
@@ -80,7 +80,29 @@ function findProduct(id, quantity){
 	});
 };
 
-function purchase(id, quantity){
-	console.log("Processing your purchase.");
+//Funtion that process the purchase and updates stock_quantity in bamazon_DB with newQtty
+function updateQuantity(id, quantity){
+	//New quantity to be update in bamazon_DB after substract the purchase
+	newQtty = inventory - quantity;
+	var query = connection.query('UPDATE products SET ? WHERE ?',
+	[
+		{
+			stock_quantity: newQtty
+		},
+		{
+			item_id: id
+		}
+	],
+	function(err, res){
+		costOfPurchase(id, quantity);
+	});
 };
-	
+
+//Function that display to the client the amount of the purchase
+var costOfPurchase = function(id, quantity){
+	var query = connection.query('SELECT price FROM products WHERE item_id=?', [id], function(err, res) {
+		cost = parseFloat(JSON.stringify(res[0].price)).toFixed(4);
+		console.log("Your total is $ " + quantity*cost);
+		allProducts();
+	});
+};
